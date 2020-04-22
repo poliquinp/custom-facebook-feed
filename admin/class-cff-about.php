e69da2a61dece588361227debfd76b171c328a53
John Brackett <hey@smashballoon.com>
2020-04-21 20:44:21 -0500

    Fixed about page not displaying plugins

--- admin/class-cff-about.php
+++ admin/class-cff-about.php
@@ -288,7 +288,7 @@ class CFF_About {
 	 */
 	protected function output_about_addons() {
 
-		if ( ! current_user_can( 'manage_instagram_feed_options' ) || 	version_compare( PHP_VERSION,  '5.3.0' ) <= 0 ) {
+		if ( ! current_user_can( 'manage_custom_facebook_feed_options' ) || version_compare( PHP_VERSION,  '5.3.0' ) <= 0 ) {
 			return;
 		}
 
